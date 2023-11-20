2bed3e1dca0230ecab0225e93f717e7ff75aa606
chandrasekhar <chandrasekharreddyanam.sitm@gmail.com>
2023-11-21 04:07:31 +0530

    Included DTO

---
+++ src/main/java/net/javaguides/springboot/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package net.javaguides.springboot.mapper;
+
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+
+public class UserMapper {
+
+    //Convert UserDto into User Jpa Entity as we will be saving user Jpa Entity object in to table/dn
+
+    public static User mapToUser(UserDto userDto){
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        return user;
+    }
+
+    //Convert User JPA entity to UserDto as we will be displaying in the response entity
+    public static UserDto mapToUserDto(User user){
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+        return userDto;
+    }
+}
--- src/main/java/net/javaguides/springboot/service/Impl/UserServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/Impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot.service.Impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.dto.UserDto;
 import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.mapper.UserMapper;
 import net.javaguides.springboot.repository.UserRepository;
 import net.javaguides.springboot.service.UserService;
 import org.springframework.stereotype.Service;
@@ -16,19 +17,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto createUser(UserDto userDto) {
         //Convert UserDto into User Jpa Entity as we will be saving user Jpa Entity object in to table/dn
-        User user=new User();
-        user.setId(userDto.getId());
-        user.setFirstName(userDto.getFirstName());
-        user.setLastName(userDto.getLastName());
-        user.setEmail(userDto.getEmail());
+        User user= UserMapper.mapToUser(userDto);
         User savedUser = userRepository.save(user);
 
         //Convert User JPA entity to UserDto
-        UserDto savedUserDto=new UserDto();
-        savedUserDto.setId(savedUser.getId());
-        savedUserDto.setFirstName(savedUser.getFirstName());
-        savedUserDto.setLastName(savedUser.getLastName());
-        savedUserDto.setEmail(savedUser.getEmail());
+        UserDto savedUserDto=UserMapper.mapToUserDto(user);
         return savedUserDto;
     }
 }
